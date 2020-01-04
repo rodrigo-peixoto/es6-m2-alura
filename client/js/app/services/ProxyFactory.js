d30f588ee41239cae9f231e0415834c776955abe
Rodrigo Peixoto <rodrigo.peixoto@fatec.sp.gov.br>
2020-01-04 12:28:35 -0300

    implementacao importacao negociacoes api

--- client/js/app/services/ProxyFactory.js
+++ client/js/app/services/ProxyFactory.js
@@ -15,6 +15,7 @@ class ProxyFactory{
             },
             set(target, prop, value, receiver){
                 if(props.includes(prop)){
+                    target[prop] = value;
                     acao(target);
                 }
                 return  Reflect.set(target, prop, value, receiver);               
